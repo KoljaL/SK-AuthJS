3dfea1425cdeeca10f3bfb9e519b8d3558bca9ee
Kolja <lasar@rasal.de>
2022-12-16 12:21:49 +0100

    init

---
+++ resetdb.sh
@@ -0,0 +1,12 @@
+rm prisma/dev.db
+rm -r prisma/migrations
+npx prisma migrate dev --name init
+
+npm run dev
+
+
+# rm prisma/dev.db-journal
+# npx prisma generate
+# npx prisma migrate dev --name init --skip-seed
+# npx prisma migrate dev --name init
+# npx prisma db seed
